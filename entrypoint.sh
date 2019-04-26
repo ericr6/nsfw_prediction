d51f8eb8e45d3ecca7977d8d19805f7a165e80ee
eric rodriguez <er@iex.ec>
2019-04-26 13:49:52 +0200

    clean entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
-echo "args 1 is " $1
 
+echo "file to download is " $1
 wget $1
 input=$(basename $1)
-cp $input /iexec_out/.
+#cp $input /iexec_out/.
 
-#Download the model as a dataset
+#Download (unzip) the model as a dataset
 ls /iexec_in
 cp /iexec_in/nsfw_model.zip /.
 unzip nsfw_model.zip
 
-#wget https://cdn.pixabay.com/photo/2016/10/17/07/53/busan-night-scene-1747130_960_720.jpg
-#wget https://cdn.pixabay.com/photo/2018/01/13/19/39/fashion-3080644_960_720.jpg
 
-echo "file download from " $1
-echo "file name is " $input
+#echo "file download from " $1
+#echo "file name is " $input
 python classify_nsfw.py $input >>/iexec_out/result.log
-echo "file download from " $1
-echo "file name is " $input
+#echo "file download from " $1
+#echo "file name is " $input
